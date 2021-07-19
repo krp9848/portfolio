6dc0e189dabc1e8a8fbb22f5a4c362066faa2c7a
krp9848 <kabirajpant@gmail.com>
2021-07-19 18:20:06 +0530

    header completed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import './Header.scss'
+import profile from './profile.jpeg'
 
 const Header = () => {
-  return <header className="header">Header Element</header>
+  return (
+    <header className="header">
+      <div className="img-container">
+        <img src={profile} alt="kabiraj" />
+      </div>
+    </header>
+  )
 }
 
 export default Header
