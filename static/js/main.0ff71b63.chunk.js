13af74514ccb7a3920c9b47046b54424e01b6b49
krp9848 <kabirajpant@gmail.com>
2021-07-19 17:56:13 +0530

    Updates

--- static/js/main.0ff71b63.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpportfolio=this.webpackJsonpportfolio||[]).push([[0],{10:function(t,e,o){"use strict";o.r(e);o(1);var n=o(3),r=o.n(n),i=(o(8),o(0));var c=function(){return Object(i.jsx)("div",{children:Object(i.jsx)("h1",{children:"Portfolio website"})})};r.a.render(Object(i.jsx)(c,{}),document.getElementById("root"))},8:function(t,e,o){}},[[10,1,2]]]);
-//# sourceMappingURL=main.0ff71b63.chunk.js.map
