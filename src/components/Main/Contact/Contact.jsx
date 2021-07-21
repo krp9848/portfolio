bdd24b5c02e37cba16e5f97ae87dee19a528ace2
krp9848 <kabirajpant@gmail.com>
2021-07-21 16:48:11 +0530

    feat: add contact details

--- src/components/Main/Contact/Contact.jsx
+++ src/components/Main/Contact/Contact.jsx
@@ -1,8 +1,77 @@
 import React from 'react'
 import './Contact.scss'
+import { FaGithub, FaInstagram, FaLinkedinIn, FaTwitter } from 'react-icons/fa'
 
 const Contact = () => {
-  return <h1>Contact</h1>
+  return (
+    <section className="section contact-section">
+      <div className="contact-form-container">
+        <div className="contact-form-heading">
+          <h2>Get In Touch</h2>
+        </div>
+        <form action="#" className="contact-form">
+          <div className="form-group">
+            <textarea
+              name="message"
+              placeholder="Please enter your message"
+              id="message"
+              cols="30"
+              rows="10"
+            ></textarea>
+          </div>
+          <a href="mailto: abc@example.com" target="_blank" rel="noreferrer">
+            Send Message
+          </a>
+        </form>
+      </div>
+
+      <div className="social-media-container">
+        <h3>Connect with me</h3>
+        <ul className="media-list">
+          <li className="media-list-item">
+            <a
+              href="https://twitter.com/pantkabiraj"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaTwitter />
+              <span>Twitter</span>
+            </a>
+          </li>
+          <li className="media-list-item">
+            <a
+              href="https://github.com/krp9848"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaGithub />
+              <span>GitHub</span>
+            </a>
+          </li>
+          <li className="media-list-item">
+            <a
+              href="https://www.linkedin.com/in/kabi-raj-pant-173a2217b/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaLinkedinIn />
+              <span>LinkedIn</span>
+            </a>
+          </li>
+          <li className="media-list-item">
+            <a
+              href="https://www.instagram.com/kabirajpant/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaInstagram />
+              <span>Instagram</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </section>
+  )
 }
 
 export default Contact
